e40f88b8148cfb10abbe76d19fb642bdaecf6f2c
erochan <yellow531@kawamuranoMacBook-Pro.local>
2022-08-02 11:14:23 +0900

    created,updated_at を日本時間に設定

--- config/application.rb
+++ config/application.rb
@@ -1,7 +1,7 @@
+
 require_relative "boot"
 
 require "rails/all"
-
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
@@ -10,6 +10,7 @@ module RailwayApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.1
+    config.time_zone = 'Tokyo'
     config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
