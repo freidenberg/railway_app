4ade9b67305c7da0ed08efce3f7f1f6922d0b740
erochan <yellow531@kawamuranoMacBook-Pro.local>
2022-09-06 14:57:04 +0900

    lines_tag_uploader追加,seeds.rb追加(*投稿、ログアウト時エラー発生)

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     belongs_to :user
     mount_uploaders :image, ImageUploader #_uploaders←複数形
+    mount_uploaders :line_tag_image, ImageUploader 
     serialize :image,JSON
     has_many_attached :image
     has_many :comments, dependent: :destroy 
