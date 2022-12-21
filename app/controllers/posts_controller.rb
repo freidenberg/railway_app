800ae1713e6f0c570f777b8e1523628f6cc958b8
erochan <yellow531@kawamuranoMacBook-Pro.local>
2022-12-21 11:18:18 +0900

    投稿編集画面作成(現在編集反映しようとするとエラー)

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,12 +13,24 @@ class PostsController < ApplicationController
           render 'new'
        end
     end
+
     def edit
-      @user.image.cache! unless @user.image.blank?
+      #@user.image.cache! unless @user.image.blank? 
+      @post = Post.find(params[:id])
     end
+
+    def update
+      @post = Post.find(params[:id])
+    # 編集ページの送信ボタンから飛んできたときのparamsに格納されたidを元に、該当する投稿データを探して、変数に代入する
+      if @post.update(post_path)
+        redirect_to post_path, notice: "投稿を編集しました"
+      else
+        flash.now[:danger] = "編集に失敗しました"
+        render 'edit'
+      end
   private
   def post_params #ストロングパラメーター
     params.require(:post).permit(:body,{image: []},:image_cache,lines_tag_ids:[],genre_tag_ids:[]) 
   end
 end  
-  
+end   
